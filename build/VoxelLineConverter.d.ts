5f3a9cab842381945e744c36f90e70b7afba58af
sunxueqing <sunxueqing@baidu.com>
2020-04-30 21:19:19 +0800

    update

---
+++ build/VoxelLineConverter.d.ts
@@ -0,0 +1,11 @@
+import VoxelChunk from "./VoxelChunk";
+export default class VoxelLineConverter {
+    static canvas: HTMLCanvasElement;
+    static convert: (image: HTMLImageElement, width?: number | undefined, height?: number | undefined, depth?: number, options?: {
+        x?: number | undefined;
+        y?: number | undefined;
+        width?: number | undefined;
+        height?: number | undefined;
+    } | undefined) => false | VoxelChunk;
+    static createChunk(imgData: Uint8ClampedArray, width: number, height: number, depth: number): VoxelChunk;
+}
